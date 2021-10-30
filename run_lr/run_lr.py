2c2432d23db7e63a90fc0d1ce5fa13bf5b7c1c12
Pavel Gorshenin <mr.gorshenya.job@gmail.com>
2021-10-30 17:44:04 +0300

    regression runner added

--- run_lr/run_lr.py
+++ run_lr/run_lr.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 from scipy.sparse.csr import csr_matrix
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Ridge, LogisticRegression
 from typing import Tuple, Union
-from logging_utils import common_logging
+from .logging_utils import common_logging
 
 
 def _get_init_data(train, val, kfold, train_labels, extra_params):
@@ -22,11 +22,11 @@ def _get_init_data(train, val, kfold, train_labels, extra_params):
 
 
 @common_logging
-def run_lr(train: csr_matrix, val: csr_matrix, train_labels: Union[pd.DataFrame, pd.Series],
-            val_labels: Union[pd.DataFrame, pd.Series], lr_params: dict,
-            log_params: dict, kfold, metric, extra_params) -> Tuple[np.ndarray]:
+def run_lr_log(train: csr_matrix, val: csr_matrix, train_labels: Union[pd.DataFrame, pd.Series],
+               val_labels: Union[pd.DataFrame, pd.Series], lr_params: dict,
+               log_params: dict, kfold, metric, extra_params) -> Tuple[np.ndarray]:
     """
-    Runs xgboost in KFold cycle
+    Runs LR in KFold cycle
 
     Simple runner. One train, one test.
     :param train: dataset to pass in kfold
@@ -42,4 +42,27 @@ def run_lr(train: csr_matrix, val: csr_matrix, train_labels: Union[pd.DataFrame,
         if val is not None:
             row_slice = np.array(range(preds_val.shape[1])).reshape(-1, 1)
             preds_val[i_fold, row_slice, lr_model.classes_] = lr_model.predict_proba(val)
+    return (preds_train, preds_val, model_lst)
+
+
+@common_logging
+def run_lr_reg(train: csr_matrix, val: csr_matrix, train_labels: Union[pd.DataFrame, pd.Series],
+            val_labels: Union[pd.DataFrame, pd.Series], lr_params: dict,
+            log_params: dict, kfold, metric, extra_params) -> Tuple[np.ndarray]:
+    """
+    Runs LR in KFold cycle
+
+    Simple runner. One train, one test.
+    :param train: dataset to pass in kfold
+    :param val: dataset to predict (no usage in training)
+    :return: preds_train, preds_val
+    """
+    preds_train, model_lst, preds_val = _get_init_data(train, val, kfold, train_labels, extra_params)
+    lr_model = Ridge(**lr_params)
+    for i_fold, (train_index, test_index) in enumerate(kfold.split(train)):
+        lr_model.fit(train[train_index], train_labels.iloc[train_index].values)
+        model_lst.append(lr_model)
+        preds_train[test_index] = lr_model.predict(train[test_index])
+        if val is not None:
+            preds_val[i_fold, :] = lr_model.predict(val)
     return (preds_train, preds_val, model_lst)
